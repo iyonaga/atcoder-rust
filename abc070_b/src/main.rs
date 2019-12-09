f148b39ee8b8cccfa45f7eee6fb02d302828b47a
iyonaga <yiyonaga@gmail.com>
2019-12-10 01:05:50 +0900

    add abc070_b

---
+++ abc070_b/src/main.rs
@@ -0,0 +1,97 @@
+use std::cmp;
+
+// Input macro
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! stdin {
+    () => {{
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        s
+    }};
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+// Test Code
+macro_rules! test {
+    ($($input:expr => $output:expr),* $(,)*) => {
+        #[test]
+        fn solve_test() {
+            $(
+                assert_eq!(solve($input), $output);
+            )*
+        }
+    };
+}
+
+test! {
+    // "0 75 25 100" => "50",
+    "0 33 66 99" => "0",
+    // "10 90 20 80" => "60",
+}
+
+fn main() {
+    println!("{}", solve(&stdin!()));
+}
+
+fn solve(src: &str) -> String {
+    input! {
+        source = src,
+        a_start: u32,
+        a_end: u32,
+        b_start: u32,
+        b_end: u32,
+    }
+
+    let start = cmp::max(a_start, b_start);
+    let end = cmp::min(a_end, b_end);
+
+    cmp::max(end as i32 - start as i32, 0).to_string()
+}
